f0ca102946222043044d222ec5ec9b20654560a7
Muzaffer Mert Akkan <m.mert.akkan@gmail.com>
2025-09-28 18:53:24 +0300

    Update ApiGatewayApplication.java

--- api-gateway/src/main/java/com/ecommercehub/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ecommercehub/gateway/ApiGatewayApplication.java
@@ -1,4 +1,19 @@
 package com.ecommercehub.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Spring Cloud Gateway Application
+ *
+ * @EnableDiscoveryClient: Enables service discovery to find other microservices
+ * Spring Cloud Gateway is built on Spring WebFlux (reactive programming)
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
